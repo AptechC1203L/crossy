8901e99db71da2be1c617d6e53e768ea46894199
Trung Ngo <ndtrung4419@gmail.com>
2013-06-05 21:06:22 +0000

    Make the Board behave like a 2-dimension array of bool

--- src/cross/Board.java
+++ src/cross/Board.java
@@ -9,11 +9,44 @@ package cross;
  * @author chin
  */
 public class Board {
-    public int width;
-    public int height;
+    private int width;
+    private int height;
+    private boolean[][] board;
 
     public Board(int width, int height) {
         this.width = width;
+        this.height = height;
+    }
+    
+    public boolean get(int x, int y) {
+        return board[x][y];
+    }
+
+    /**
+     * @return the width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(int height) {
         this.height = height;
     }
 }
