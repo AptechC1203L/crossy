81750e7099301a821b2adbadcb93cbe6e6894f19
Trung Ngo <ndtrung4419@gmail.com>
2013-06-05 22:45:26 +0000

    Implement Player

--- src/cross/Player.java
+++ src/cross/Player.java
@@ -4,14 +4,33 @@
  */
 package cross;
 
+import java.util.Scanner;
+
 /**
  *
  * @author chin
  */
 public class Player {
+
+    private char signature;
+    private String name;
+
+    public Player(char signature, String name) {
+        this.signature = signature;
+        this.name = name;
+    }
+
     public Turn makeAMove() {
         // TODO randomize or ask user
-        Turn t = new Turn(3, 4, null);
+        System.out.println("Hey, make a move, " + this.name + ":");
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        Turn t = new Turn(x, y, this);
         return t;
+    }
+
+    public char getSignature() {
+        return this.signature;
     }
 }
